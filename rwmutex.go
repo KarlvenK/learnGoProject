01a86514e8cce23ac8de18c5e0b84881d6d1e7fb
Karlven <karlven@outlook.com>
2021-03-27 11:38:27 +0800

    =

--- rwmutex.go
+++ rwmutex.go
@@ -14,17 +14,21 @@ func tryRWMutex(kkkk int) {
 	lock := &sync.RWMutex{}
 	lock.Lock()
 
+	var wg sync.WaitGroup
+	wg.Add(4)
 	for i := 0; i < 4; i++ {
 		go func(i int) {
 			fmt.Printf("the %vth goroutine prepare to start...\n", i)
+			wg.Done()
 			lock.RLock()
+			//wg.Done()
 			fmt.Printf("the %vth goroutine got read mutex, after 1 second sleep, unlock the locker\n", i)
 			time.Sleep(time.Second)
 			lock.RUnlock()
 		}(i)
 	}
+	wg.Wait()
 
-	time.Sleep(time.Second * 2)
 	fmt.Println("prepare unlock the wmutex. rmutex stop deadloop")
 	lock.Unlock()
 
