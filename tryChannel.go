3eaced6780553f0a9e29b6426eee0e44eca3cc10
Karlven <karlven@outlook.com>
2021-03-27 11:33:50 +0800

    deadlock example

--- tryChannel.go
+++ tryChannel.go
@@ -7,24 +7,33 @@ func tryChannel(v int) {
 		return
 	}
 	/*
-	arr := []int {1, 2, 3, 4, 5, 6, 7}
-	n := len(arr)
-	c := make(chan int)
-	go sum(arr[:n], c)
-	go sum(arr[:n/2], c)
-	x, y := <-c, <- c
-	fmt.Println(x, y)
+		arr := []int {1, 2, 3, 4, 5, 6, 7}
+		n := len(arr)
+		c := make(chan int)
+		go sum(arr[:n], c)
+		go sum(arr[:n/2], c)
+		x, y := <-c, <- c
+		fmt.Println(x, y)
 
-	fmt.Println("========")
+		fmt.Println("========")
 
-	ch := make(chan int, 45)
-	go putNum(cap(ch), ch)
-	for i := range ch {
-		fmt.Println(i)
-	}
+		ch := make(chan int, 45)
+		go putNum(cap(ch), ch)
+		for i := range ch {
+			fmt.Println(i)
+		}
 	*/
 	testTree()
-
+	fmt.Println("testTree end")
+	pipeline := make(chan string)
+	go func() {
+		defer close(pipeline) //without it the goroutine will deadlock
+		pipeline <- "helloworld"
+		pipeline <- "hellochina"
+	}()
+	for data := range pipeline {
+		fmt.Println(data)
+	}
 }
 
 func testTree() {
@@ -36,6 +45,7 @@ func testTree() {
 	}
 	fmt.Println(Same(t1, t1))
 }
+
 // Walk 步进 tree t 将所有的值从 tree 发送到 channel ch。
 func Walk(t *Tree, ch chan int) {
 	realWalk(t, ch)
@@ -49,18 +59,19 @@ func realWalk(t *Tree, ch chan int) {
 	ch <- t.Value
 	realWalk(t.Right, ch)
 }
+
 // Same 检测树 t1 和 t2 是否含有相同的值。
 func Same(t1, t2 *Tree) bool {
 	ch1, ch2 := make(chan int), make(chan int)
 	go Walk(t1, ch1)
-	go Walk(t2,ch2)
+	go Walk(t2, ch2)
 
 	for i := range ch1 {
-		if i != <- ch2 {
+		if i != <-ch2 {
 			return false
 		}
 	}
-	return  true
+	return true
 }
 
 /*
